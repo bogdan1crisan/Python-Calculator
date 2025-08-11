3778162a7ff72f769cf28acde56d9f7f79bed6bc
buogdi <crisanbogdy@gmail.com>
2025-08-11 17:22:28 +0300

    finished calculator

--- Untitled-1.py
+++ Untitled-1.py
@@ -16,3 +16,40 @@ def divide(a, b):
     else:
         answer = a / b
         print (a , " / " , b , " = " , answer)
+
+while True:
+    print("\n")
+    print("Calculator!")
+    print("A. Add")
+    print("B. Subtract")
+    print("C. Multiply")
+    print("D. Divide")
+    print("E. Exit")
+
+    choice= input("Enter your choice (A-E): ")
+    print("\n")
+    if choice == 'A' or choice == 'a':
+        print("You chose addition")
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        add(a, b)
+    elif choice == 'B' or choice == 'b':
+        print("You chose subtraction")
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        subtract(a, b) 
+    elif choice == 'C' or choice == 'c':
+        print("You chose multiplication")
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        multiply(a, b)
+    elif choice == 'D' or choice == 'd':
+        print("You chose division")
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        divide(a, b)
+    elif choice == 'E' or choice == 'e':
+        print("Exiting the calculator. Goodbye!")
+        quit()
+    else:
+        print("Invalid choice. Choose between A-E.")
